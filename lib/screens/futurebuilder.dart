8e1b3f9ba398290218075202b6481fdd09bdb5bc
Harshpreet Singh <harshpreet.singh@successive.com>
2023-12-19 16:20:57 +0530

    19/12/23

---
+++ lib/screens/futurebuilder.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:streambuilder/screens/streambuilder.dart';
+
+class FutureBuilderPage extends StatefulWidget {
+  const FutureBuilderPage({super.key});
+
+  @override
+  State<FutureBuilderPage> createState() => _FutureBuilderPageState();
+}
+
+class _FutureBuilderPageState extends State<FutureBuilderPage> {
+  Future<String> getData() {
+    return Future.delayed(const Duration(seconds:1), () {
+      return 'Data';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Colors.white,
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FutureBuilder(
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return Text('${snapshot.error} has occurred');
+                    } else if (snapshot.hasData) {
+                      return Text('${snapshot.data}',style:const TextStyle(fontSize: 25),);
+                    }
+                  }
+                  return const CircularProgressIndicator();
+                },
+              ),
+             const SizedBox(height: 40,),
+              const StreamBuilderPage()
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/screens/streambuilder.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class StreamBuilderPage extends StatefulWidget {
+  const StreamBuilderPage({super.key});
+
+  @override
+  State<StreamBuilderPage> createState() => _StreamBuilderPageState();
+}
+
+class _StreamBuilderPageState extends State<StreamBuilderPage> {
+  late StreamController<int> streamController;
+  late StreamController streamController2;
+
+  @override
+  void initState() {
+    super.initState();
+    streamController2 = StreamController();
+    wait();
+    streamController = StreamController<int>(
+      onListen: () async {
+        await Future.delayed(const Duration(seconds: 4));
+        for (int i = 0; i <= 10; i++) {
+          await Future.delayed(const Duration(seconds: 1));
+          streamController.add(i);
+        }
+      },
+    );
+  }
+
+  void wait() async {
+    await Future.delayed(const Duration(seconds: 5));
+    streamController2.sink.add(1);
+  }
+
+  
+
+  @override
+  void dispose() {
+    streamController.close();
+    streamController2.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.white,
+          alignment: Alignment.center,
+          child: StreamBuilder(
+            stream: streamController2.stream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          color: Colors.white,
+          alignment: Alignment.center,
+          child: StreamBuilder<int>(
+            stream: streamController.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error : ${snapshot.error}');
+              } else if (!snapshot.hasData) {
+                return const Text('There is no Data');
+              } else {
+                return ElevatedButton(
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+                  onPressed: null,
+                  child: Text(
+                    'Counting: ${snapshot.data}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
